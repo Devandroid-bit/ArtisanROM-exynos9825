0ef5f0b5eb0467f79e267540e118f702d7dac7c8
Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>
2026-09-19 14:56:09 +0100

    exynos9825: midas: disable MIDAS model detection
    Comment out the MIDAS model detection logging and sed commands.
    
    Signed-off-by: Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>

--- platform/exynos9825/patches/midas/customize.sh
+++ platform/exynos9825/patches/midas/customize.sh
@@ -5,9 +5,9 @@ ADD_TO_WORK_DIR "r9sxxx" "vendor" "etc/midas"
 ADD_TO_WORK_DIR "r9sxxx" "vendor" "etc/VslMesDetector"
 LOG_STEP_OUT
 
-LOG "- Fixing MIDAS model detection"
-sed -i "s/$SOURCE_CODENAME/dummy/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
-sed -i "s/r9s/$SOURCE_CODENAME/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
+#LOG "- Fixing MIDAS model detection"
+#sed -i "s/$SOURCE_CODENAME/dummy/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
+#sed -i "s/r9s/$SOURCE_CODENAME/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 
 LOG_STEP_IN "- Adding S21 FE (r9sxxx) Photo Remaster Service"
 ADD_TO_WORK_DIR "r9sxxx" "system" "system/priv-app/PhotoRemasterService/PhotoRemasterService.apk"
