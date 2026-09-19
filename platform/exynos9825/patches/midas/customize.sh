ff0cad8a104609d88b8cbb7e24e0ede56031fa8f
Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>
2026-09-19 15:41:22 +0100

    exynos9825: patches: midas: update MIDAS model detection logging and commands
    Signed-off-by: Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>

--- platform/exynos9825/patches/midas/customize.sh
+++ platform/exynos9825/patches/midas/customize.sh
@@ -5,11 +5,11 @@ ADD_TO_WORK_DIR "r9sxxx" "vendor" "etc/midas"
 ADD_TO_WORK_DIR "r9sxxx" "vendor" "etc/VslMesDetector"
 LOG_STEP_OUT
 
-#LOG "- Fixing MIDAS model detection"
-#sed -i "s/$SOURCE_CODENAME/dummy/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
-#sed -i "s/r9s/$SOURCE_CODENAME/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
+LOG "- Fixing MIDAS model detection"
+sed -i "s/b0s/dummy/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 
 LOG_STEP_IN "- Adding S21 FE (r9sxxx) Photo Remaster Service"
+DELETE_FROM_WORK_DIR "system" "system/priv-app/PhotoRemasterService/oat"
 ADD_TO_WORK_DIR "r9sxxx" "system" "system/priv-app/PhotoRemasterService/PhotoRemasterService.apk"
 LOG_STEP_OUT
 
